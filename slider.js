1b1df225e45fbb0fa7e8adb1b2e0c2102162bcc0
CYNO28 <44347226+CYNO28@users.noreply.github.com>
2022-03-30 20:39:08 +0530

    new

---
+++ slider.js
@@ -0,0 +1,7 @@
+$('.slick-carousel').slick({
+    infinite: true,
+    slidesToShow: 3, // Shows a three slides at a time
+    slidesToScroll: 3, // When you click an arrow, it scrolls 1 slide at a time
+    arrows: true, // Adds arrows to sides of slider
+    dots: true // Adds the dots on the bottom
+  });
