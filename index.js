c01d03def89691403fa7e1ad7af65e83f27e7773
CYNO28 <44347226+CYNO28@users.noreply.github.com>
2022-04-01 15:33:07 +0530

    cyno changed

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ $('.slick-carousel').slick({
     arrows: true, // Adds arrows to sides of slider
     dots: true // Adds the dots on the bottom
   });
+  function freshlybusinessHandler(){
+    window.location.href='freshlybusiness.html'
+  }
+
+    var img=document.querySelector(".nav_top_logo2>img")
+    img.addEventListener('mouseover',function(){
+
+      img.setAttribute( "src",'https://assets-global.website-files.com/5d03b4e130118314af624b20/5f7b2e54745410836cfa3955_bizlogo_main-07.svg')  
+    })
+    img.addEventListener('mouseleave',function(){
+      
+      img.setAttribute( "src",'https://assets-global.website-files.com/5d03b4e130118314af624b20/5f7b2e5362c26e009ed80a5a_bizlogo_gray-09.svg')  
+    })
