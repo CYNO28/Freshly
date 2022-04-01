f167ffdb5de4d3300039fd283ff48fb7133caccf
CYNO28 <44347226+CYNO28@users.noreply.github.com>
2022-04-01 16:15:37 +0530

    finally done

--- refe.js
+++ refe.js
@@ -7,11 +7,15 @@ function openPop(){
   event.preventDefault();
   popup.style.display = "Block";
 }
-
 window.addEventListener("click", closePop);
+document.querySelector('.close_raf>svg').addEventListener("click",function(){
+  popup.style.display = "none";
+
+})
 
 function closePop(e){
   if(e.target == popup) {
+    
   popup.style.display = "none";
 }
 }
